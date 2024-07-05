0f27c01f45b87c0e19eae05efb76caee0220add9
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-07-05 01:32:34 -0300

    feat: list page and show page styled

---
+++ db/schema.rb
@@ -0,0 +1,11 @@
+ActiveRecord::Schema[7.1].define(version: 2024_07_05_032003) do
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
